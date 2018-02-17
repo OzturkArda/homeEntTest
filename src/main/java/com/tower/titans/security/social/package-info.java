c5391288d1fee132e3ef8b81db605c5cb6aa1556
Arda Öztürk <ardaozturk@Ardas-MacBook-Pro.local>
2018-02-17 12:01:26 +0300

    First Commit

---
+++ src/main/java/com/tower/titans/security/social/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring social configuration.
+ */
+package com.tower.titans.security.social;
---
+++ src/main/webapp/app/shared/shared-common.module.ts
@@ -0,0 +1,42 @@
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import locale from '@angular/common/locales/en';
+
+import {
+    GipsterSharedLibsModule,
+    JhiLanguageHelper,
+    FindLanguageFromKeyPipe,
+    JhiAlertComponent,
+    JhiAlertErrorComponent
+} from './';
+
+@NgModule({
+    imports: [
+        GipsterSharedLibsModule
+    ],
+    declarations: [
+        FindLanguageFromKeyPipe,
+        JhiAlertComponent,
+        JhiAlertErrorComponent
+    ],
+    providers: [
+        JhiLanguageHelper,
+        Title,
+        {
+            provide: LOCALE_ID,
+            useValue: 'en'
+        },
+    ],
+    exports: [
+        GipsterSharedLibsModule,
+        FindLanguageFromKeyPipe,
+        JhiAlertComponent,
+        JhiAlertErrorComponent
+    ]
+})
+export class GipsterSharedCommonModule {
+    constructor() {
+        registerLocaleData(locale);
+    }
+}
